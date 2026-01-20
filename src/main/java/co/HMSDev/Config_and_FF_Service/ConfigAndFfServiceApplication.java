05e8fc2aad1b08d6ed92987a3777372c1decf6d5
Hawa <hsylla@manh.com>
2026-01-20 11:14:09 -0500

    Initial Commit

---
+++ src/main/java/co/HMSDev/Config_and_FF_Service/ConfigAndFfServiceApplication.java
@@ -0,0 +1,13 @@
+package co.HMSDev.Config_and_FF_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigAndFfServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigAndFfServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/HMSDev/Config_and_FF_Service/ConfigAndFfServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.HMSDev.Config_and_FF_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigAndFfServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
